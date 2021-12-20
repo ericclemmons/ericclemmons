849cb6dde1c8301640d2dbab5c8db201857c5715
Eric Clemmons <eric@smarterspam.com>
2021-12-19 17:23:14 -0800

    Use convertkit for subscribers

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -27,7 +27,7 @@ const siteMetadata = {
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo
     // Please add your .env file and modify it according to your selection
-    provider: 'buttondown',
+    provider: 'convertkit',
   },
   comment: {
     // Select a provider and use the environment variables associated to it
