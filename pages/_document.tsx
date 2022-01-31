5227f4ef75e564e0c30c2e8aeaab8b058b3ee60d
Eric Clemmons <eric@smarterspam.com>
2022-01-30 20:30:17 -0600

    Fix 100vw overflow

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -55,7 +55,7 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
           /> */}
         </Head>
-        <body className="antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
+        <body className="overflow-x-hidden antialiased text-black bg-white dark:bg-gray-900 dark:text-white">
           <Main />
           <NextScript />
         </body>
