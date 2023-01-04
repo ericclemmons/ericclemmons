003da39b0b673b207c124714eac3a954c4d9e52c
Eric Clemmons <eric@smarterspam.com>
2023-01-04 17:42:42 -0600

    Update header

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -1,7 +1,7 @@
 export function SocialLink({ icon: Icon, ...props }) {
   return (
     <a className="group -m-1 p-1" {...props}>
-      <Icon className="h-6 w-6 text-black transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
+      <Icon className="h-6 w-6 text-black transition group-hover:fill-zinc-600 dark:fill-zinc-100 dark:group-hover:fill-white" />
     </a>
   )
 }
