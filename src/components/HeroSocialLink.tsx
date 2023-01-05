bc5bf1e41e31ce7f7d349d4616b3dd7813245187
Eric Clemmons <eric@smarterspam.com>
2023-01-04 18:12:52 -0600

    /about page

--- src/components/HeroSocialLink.tsx
+++ src/components/HeroSocialLink.tsx
@@ -1,4 +1,4 @@
-export function SocialLink({ icon: Icon, ...props }) {
+export function HeroSocialLink({ icon: Icon, ...props }) {
   return (
     <a className="group -m-1 p-1" {...props}>
       <Icon className="h-6 w-6 text-black transition group-hover:fill-zinc-600 dark:fill-zinc-100 dark:group-hover:fill-white" />
