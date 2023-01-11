32122e5d3ae90e7554dd0e2001f1fb3105c3e9b7
Eric Clemmons <eric@smarterspam.com>
2023-01-11 00:09:10 -0600

    Tweet embed

--- src/components/MDX.tsx
+++ src/components/MDX.tsx
@@ -1,10 +1,3 @@
-import { Tweet } from 'mdx-embed/dist/components/twitter'
-
+export { default as Tweet } from '@/components/Tweet.astro'
 export { default as CodeSandbox } from '@/components/CodeSandbox.astro'
 export { Gist } from 'mdx-embed/dist/components/gist'
-
-export function Tweet(props) {
-  return (
-    <Tweet align="center" hideConversation={true} theme="dark" {...props} />
-  )
-}
