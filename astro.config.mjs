5e713358c754588cb65dc25579de961514c569f4
Eric Clemmons <eric@smarterspam.com>
2023-01-11 10:56:27 -0600

    Use static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from '@astrojs/mdx'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/static'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ericclemmons.com/',
   experimental: {
     contentCollections: true,
   },
@@ -20,6 +21,6 @@ export default defineConfig({
       serviceEntryPoint: '@astrojs/image/sharp',
     }),
   ],
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
 })
