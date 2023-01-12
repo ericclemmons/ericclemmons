cdcf78c24dfd957c85ec0fb092b13dddad21a363
Eric Clemmons <eric@smarterspam.com>
2023-01-11 22:09:45 -0600

    Converting more to .astro

--- src/components/HeroSocialLink.tsx
+++
@@ -1,7 +0,0 @@
-export function HeroSocialLink({ icon: Icon, ...props }) {
-  return (
-    <a className="group -m-1 p-1" {...props}>
-      <Icon className="h-6 w-6 text-black transition group-hover:fill-zinc-600 dark:fill-zinc-100 dark:group-hover:fill-white" />
-    </a>
-  )
-}
