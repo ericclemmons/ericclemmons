1c8b20ca78493d1c9791ced4b66d1ced2140d7c5
Eric Clemmons <eric@smarterspam.com>
2024-01-01 18:56:03 -0600

    Fix lint errors

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@ import { z, defineCollection } from 'astro:content'
 
 const blog = defineCollection({
   schema: z.object({
-    category: z.enum(['astro']).optional(),
+    category: z.enum(['astro', 'webpack']).optional(),
     date: z.date(),
     description: z.undefined(), // Old value that shouldn't be used anymore
     summary: z.string().optional(),
